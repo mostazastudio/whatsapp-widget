62ab5f67fe81c4fac437dc1a19eeefc813aa2bb1
Mateo Peralta <mperalta@mostaza.com.co>
2021-09-23 13:50:03 -0500

    paquete2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import InitialButton from './components/InitialButton';
 import WhatsappForm from './components/WhatsappForm';
 import "./App.css"
 
 const App = () => {
+
+    useEffect(()=>{
+        var queryString = window.location.search;
+        var urlParams   = new URLSearchParams(queryString);
+        var source = urlParams.get("utm_source")
+        console.log(source)
+    },[])
+
     return (
         <div className="container">
             <WhatsappForm></WhatsappForm>
