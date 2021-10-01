1887f2632ae8cae7eef55207ad455146ba80d688
Mateo Peralta <mperalta@mostaza.com.co>
2021-10-01 18:55:29 -0500

    cambios

--- src/context/loginContext.js
+++ src/context/loginContext.js
@@ -6,27 +6,54 @@ const LoginContext = createContext();
 const LoginProvider = ({children}) => {
 
     const [token, setToken] = useState(localStorage.getItem("whatsappWidgetToken"));
+    const [errorMessage, setErrorMessage] = useState("")
     
-    useEffect(()=>{
-        const fetchUser = async () =>{
-            const requestOptions = {
-                method: "GET",
-                headers: {
-                    "Content-Type": "application/json",
-                    Authorization : "Bearer " + token,
-                },
-            };
+    const fetchUser = async () =>{
+        const requestOptions = {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization : "Bearer " + token,
+            },
+        };
 
-            const response = await fetch("http://127.0.0.1:8000/marcas/me",requestOptions);
+        const response = await fetch("http://127.0.0.1:8000/marcas/me",requestOptions);
 
-            if ( !response.ok) {
-                setToken(null);
-            }
-
+        if (!response.ok) {
+            setToken(null);
             localStorage.setItem("whatsappWidgetToken", token)
+            return (false)
+        }else{
+            return(true)
         }
 
-        fetchUser();
+        
+        
+    }
+
+    const fetchToken = async () => {
+        const requestOptions = {
+            method: "POST",
+            headers: { "Content-Type": "application/json"},
+            body: JSON.stringify({usename:"localhost",password:"Olvidala249ever24**85"})
+        };
+
+        const response = await fetch("http://127.0.0.1:8000/marcas/token", requestOptions);
+        const data = await response.json();
+
+        if(!response.ok){
+            setErrorMessage(data.detail)
+        }else{
+            setToken(data.access_token)
+        }
+    }
+
+    useEffect(()=>{
+
+        token_valid = fetchUser();
+        if(!token_valid){
+            fetchToken()
+        }
     }, [token]);
 
     const data = {token, setToken}
